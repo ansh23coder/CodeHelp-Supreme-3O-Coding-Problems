db8e53e6670dbc6a1b80a629f27b3947734a9a34
Anupam Sharma <ansh151923@gmail.com>
2025-07-07 10:21:05 +0530

    add gfg distBtw2Points.cpp

---
+++ 0leetCode_HackerRank_Gfg/gfg_DistanceBetweenTwoPoints.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/batch/fork-cpp/track/module-1-basics-and-control-structures/problem/distance-between-2-points3200
+
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int distance(int x1, int y1, int x2, int y2) {
+        double dist = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+        return ceil(dist);  // Round up to nearest integer
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.distance(0, 0, 2, -2) << endl;     // Output: 3
+    cout << obj.distance(-20, 23, -15, 68) << endl; // Output: 45
+    return 0;
+}
+
+*/
