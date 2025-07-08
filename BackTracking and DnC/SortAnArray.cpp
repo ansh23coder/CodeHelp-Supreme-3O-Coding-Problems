baa158553558fdc3609753e61f52f618ad5e318e
Anupam Sharma <ansh151923@gmail.com>
2025-07-08 23:49:48 +0530

    updating leetcode912

--- BackTracking and DnC/SortAnArray.cpp
+++ BackTracking and DnC/SortAnArray.cpp
@@ -1,4 +1,4 @@
-// 
+// https://leetcode.com/problems/sort-an-array/
 
 // using *Priority Queue*
 /*
@@ -6,7 +6,7 @@ class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
         priority_queue<int> p;
-        
+
         for(int i=0; i<nums.size(); i++)
             p.push(nums[i]);
         
