6bea602b137f172664d044926071862ac678151f
Anupam Sharma <ansh151923@gmail.com>
2025-07-10 10:05:05 +0530

    added

---
+++ 0leetCode_HackerRank_Gfg/gfg_FloydsTriangle.cpp
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/batch/fork-cpp/track/module-1-basics-and-control-structures/problem/floyds-triangle1222
+
+/*
+class Solution {
+  public:
+    void printFloydTriangle(int n) {
+        // Write Your Code here
+        int num = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                cout << num << " ";
+                num++;
+            }
+            cout << endl;
+        }
+    }
+};
+*/
