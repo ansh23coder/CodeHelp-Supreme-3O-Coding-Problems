64a5a7709d65b87eebb89578b42fd83d5f507afe
Anupam Sharma <ansh151923@gmail.com>
2025-07-14 11:08:11 +0530

    updated

--- 0leetCode_HackerRank_Gfg/1290CnvrtBinaryNumLinkdLstIntoInteger.cpp
+++ 0leetCode_HackerRank_Gfg/1290CnvrtBinaryNumLinkdLstIntoInteger.cpp
@@ -1,16 +1,8 @@
 // https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
 
+
 /*
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// With BIT MANIPULATION
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
@@ -20,6 +12,26 @@ public:
             head = head -> next;
         }
         return ans;
+    }
+};
+
+// Without BIT MANIPULATION
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int result = head->val;
+        while (head->next) {
+            result = result * 2 + head->next->val;
+            head = head->next;
+        }
+        return result;
     }
 };
 */
