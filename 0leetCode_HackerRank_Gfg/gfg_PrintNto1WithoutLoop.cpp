3978c1382075b3ad885c1d479c7931b362ca54af
Anupam Sharma <ansh151923@gmail.com>
2025-07-14 16:22:34 +0530

    added gfg problem

---
+++ 0leetCode_HackerRank_Gfg/gfg_PrintNto1WithoutLoop.cpp
@@ -0,0 +1,15 @@
+// https://www.geeksforgeeks.org/batch/fork-cpp/track/module-recursion-file-handling/problem/print-n-to-1-without-loop
+
+/*
+class Solution {
+  public:
+    void printNos(int N) {
+        // code here
+        if(N > 0){
+            cout << N << " ";
+            printNos(N - 1);
+        }
+    }
+};
+
+*/
