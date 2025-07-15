e92c53963bf42d24786f0dbdd9945ec67f71415b
Anupam Sharma <ansh151923@gmail.com>
2025-07-15 18:25:35 +0530

    updated gfg problem

--- 0leetCode_HackerRank_Gfg/gfg_RecursiveSequence.cpp
+++ 0leetCode_HackerRank_Gfg/gfg_RecursiveSequence.cpp
@@ -1,5 +1,17 @@
 // https://www.geeksforgeeks.org/batch/fork-cpp/track/module-recursion-file-handling/problem/recursive-sequence1611
 
+/*
+A function F is defined as follows F(n)= (1) +(2*3) + (4*5*6) ... upto n terms (look at the examples for better clarity). Given an integer n, determine the F(n).
+
+Note: As the answer can be very large, return the answer modulo 109+7.
+
+Example 1:
+
+Input: n = 5
+Output: 365527
+Explanation: 
+F(5) = 1 + 2*3 + 4*5*6 + 7*8*9*10 + 11*12*13*14*15 = 365527.
+*/
 // User function Template for C++
 class Solution {
   public:
