e80b06344e1c2424593ef003ba3cef11de08cea7
Anupam Sharma <ansh151923@gmail.com>
2025-07-16 23:52:36 +0530

    adding gfg problem

---
+++ Recursion1/gfg_BinarySearch.cpp
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/binary-search-1587115620/11
+
+
+class Solution {
+  public:
+    int binarysearch(vector<int> &arr, int k) {
+        // code here
+        
+        int low = 0, high = arr.size() - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                result = mid;        
+                high = mid - 1;
+            } 
+            else if (arr[mid] < k) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+};
