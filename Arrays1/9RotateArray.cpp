e93b5a39ae9c964edc6ecb14f266788ddd167c8a
Anupam Sharma <ansh151923@gmail.com>
2025-07-23 12:30:21 +0530

    added

--- Arrays1/9RotateArray.cpp
+++ Arrays1/9RotateArray.cpp
@@ -1,14 +1,33 @@
 // https://leetcode.com/problems/rotate-array/
 
 /*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size(); // handle cases where k is greater than nums.size()
+        int n = nums.size();
+        k %= n;
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
         reverse(nums.begin() + k, nums.end());
-
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+
+    sol.rotate(nums, k);
+
+    for (int num : nums) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
+
 */
