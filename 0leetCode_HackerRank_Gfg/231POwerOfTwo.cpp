4b48be757b34762c10ce953149524852eaf6c8d9
Anupam Sharma <ansh151923@gmail.com>
2025-08-09 22:40:50 +0530

    updated

--- 0leetCode_HackerRank_Gfg/231POwerOfTwo.cpp
+++ 0leetCode_HackerRank_Gfg/231POwerOfTwo.cpp
@@ -1,10 +1,14 @@
 // https://leetcode.com/problems/power-of-two/
 
+#include <iostream>
+#include <cmath> // for pow() in cpp
+using namespace std;
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) 
     {
-        for(int i = 0; i <= 30; i++)
+        for(int i = 0; i <= 30; i++) // 2^30 = 1,073,741,824
         {
             int ans = pow(2, i);
             if(ans == n)
@@ -15,3 +19,19 @@ public:
         return false;
     }
 };
+
+int main() {
+    Solution sol;
+    int n;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    if (sol.isPowerOfTwo(n)) {
+        cout << n << " is a power of two." << endl;
+    } else {
+        cout << n << " is NOT a power of two." << endl;
+    }
+
+    return 0;
+}
