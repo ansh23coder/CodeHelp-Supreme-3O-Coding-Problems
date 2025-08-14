f50f7ceff3b56ffbadc126665037eaab878ee9a5
Anupam Sharma <ansh151923@gmail.com>
2025-08-14 22:45:36 +0530

    adding

--- Recursion1/BasicProblems/PrintNumsFrom1toN.cpp
+++ Recursion1/BasicProblems/PrintNumsFrom1toN.cpp
@@ -1,3 +1,5 @@
+// Print Numbers from 1 to N
+
 #include <iostream>
 using namespace std;
 
@@ -14,4 +16,24 @@ int main() {
 }
 /*
 n = 5 → Output: 1 2 3 4 5
+*/
+
+
+// Print Numbers from n to 1
+#include <iostream>
+using namespace std;
+
+void printNto1(int n) {
+    if (n == 0) return;
+    cout << n << " ";    // Print while going down
+    printNto1(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printNto1(n);
+}
+/*
+n = 5 → Output: 5 4 3 2 1
 */
