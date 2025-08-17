d134f9d5f00ce7eaa23d7c39d7b6886955dddbe5
Anupam Sharma <ansh151923@gmail.com>
2025-08-17 19:38:25 +0530

    adding 1323lt

---
+++ 0leetCode_HackerRank_Gfg/1323Max69Number.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/maximum-69-number/
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = list(str(num))
+        for i in range(len(str)):
+            if s[i] == '6':
+                s[i] = '9'
+                break
+        return int(''.join(s))
